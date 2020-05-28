2c1fcd7d17774f326678fc84218db20c009ee8d0
alvoltta@gmail.com <alvoltta@gmail.com>
2020-05-28 19:05:08 +0300

    Organization model added

--- Volta.ChefHelperBL/Model/Organization.cs
+++ Volta.ChefHelperBL/Model/Organization.cs
@@ -6,5 +6,18 @@ namespace Volta.ChefHelperBL.Model
 {
     class Organization
     {
+        public string Name { get; }
+        public string Comment { get; set; }
+
+        public Organization(string name, string comment)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя организации не может быть пустым");
+            }
+            Name = name;
+            Comment = comment;
+
+        }
     }
 }
